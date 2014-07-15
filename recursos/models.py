90e25170e5daad010cdc09357e3f207f08f40efb
Mi <mickawaii@gmail.com>
2014-07-15 10:03:56 -0300

    recursos index created

--- recursos/models.py
+++ recursos/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Recurso(models.Model):
-    tipo_recurso = models.CharField(max_length=50)
-    nome = models.CharField(max_length=50)
-    status = models.CharField(max_length=10)
+  tipo_recurso = models.CharField(max_length=50)
+  nome = models.CharField(max_length=50)
+  status = models.CharField(max_length=10)
 
-    def __unicode__(self):
-      return self.nome
+  def __unicode__(self):
+    return self.nome
--- recursos/views.py
+++ recursos/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You´re at the poll index.")
+	return render(request, 'recursos/index.html')
+

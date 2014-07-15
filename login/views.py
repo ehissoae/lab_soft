64d5d9d3ff42f996bcec3f51cb6224df5056498d
felipe.toyoda <kibe.felipe@gmail.com>
2014-07-15 09:30:31 -0300

    pagina de login

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return HttpResponse("EAEEEEEEEEEE")
+  return render(request, 'login/home.html')

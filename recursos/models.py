a7ac44d3c2de23ad07e9aae27e49eff38a8c9e17
felipe.toyoda <kibe.felipe@gmail.com>
2014-07-15 10:04:06 -0300

    model de recurso com telefone

--- recursos/models.py
+++ recursos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Recurso(models.Model):
-    tipo_recurso = models.CharField(max_length=50)
-    nome = models.CharField(max_length=50)
-    status = models.CharField(max_length=10)
+  tipo_recurso = models.CharField(max_length=50)
+  nome = models.CharField(max_length=50)
+  status = models.CharField(max_length=10)
+  phone_number = models.CharField(max_length=20)
 
-    def __unicode__(self):
-      return self.nome
+  def __unicode__(self):
+    return self.nome

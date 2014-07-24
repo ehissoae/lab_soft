cca927fdbfd5461583d2610e6cf611599bd02163
Mi <mickawaii@gmail.com>
2014-07-24 17:07:55 -0300

    template working, bootstrap added and login done

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Perfil(models.Model):
-    user = models.OneToOneField(User)
-    tipoAcesso = models.CharField(max_length=100)
+class Profile(models.Model):
+  ROLE_CHOICES = (
+    ("administrador", 'Administrador'),
+    ("coordenador", 'Coordenador'),
+    ("especialista", 'Especialista'),
+  )
+  user = models.OneToOneField(User)
+  tipoAcesso = models.CharField(max_length=100,
+                                choices=ROLE_CHOICES, 
+                                default="coordenador")
 
--- recursos/views.py
+++ recursos/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'recursos/index.html')
+	tipoAcesso = request.user.profile.tipoAcesso
+	return render(request, 'recursos/index.html', {"tipoAcesso":tipoAcesso})
 

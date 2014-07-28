cb1b17bccd65b600ac6f3da9d0cc8d343d072d31
felipe.toyoda <kibe.felipe@gmail.com>
2014-07-28 14:30:42 -0300

    conflitos

--- recursos/views.py
+++ recursos/views.py
@@ -3,11 +3,6 @@ from recursos.models import Recurso
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
   recursos = Recurso.objects.all()
-  return render(request, 'recursos/index.html', {'recursos': recursos})
-=======
-	tipoAcesso = request.user.profile.tipoAcesso
-	return render(request, 'recursos/index.html', {"tipoAcesso":tipoAcesso})
-
->>>>>>> cca927fdbfd5461583d2610e6cf611599bd02163
+  tipoAcesso = request.user.profile.tipoAcesso
+  return render(request, 'recursos/index.html', {"tipoAcesso":tipoAcesso, 'recursos': recursos})

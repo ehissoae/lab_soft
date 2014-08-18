39ed9b872d742a48e464dbb354232f749d6847ca
felipe.toyoda <kibe.felipe@gmail.com>
2014-08-18 19:57:22 -0300

    admin retirado

--- SiGeCAV/urls.py
+++ SiGeCAV/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from home import views
-
-admin.autodiscover()
 
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'^conta/', include('conta.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^recursos/', include('recursos.urls')),
     url(r'^acidentes/missoes/', include('missoes.urls')),
     url(r'^acidentes/', include('acidentes.urls')),

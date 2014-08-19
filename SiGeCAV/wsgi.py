a722403e13dde512874590f553d40819893c0850
felipe.toyoda <kibe.felipe@gmail.com>
2014-08-19 01:18:41 -0300

    pro master

--- SiGeCAV/wsgi.py
+++ SiGeCAV/wsgi.py
@@ -13,8 +13,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SiGeCAV.settings")
 from django.core.wsgi import get_wsgi_application
 
 # ---- HEROKU
-from dj_static import Cling
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application()) 
+# application = Cling(get_wsgi_application()) 
 # ---- HEROKU
-# application = get_wsgi_application()
+application = get_wsgi_application()
